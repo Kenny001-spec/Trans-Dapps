f222a6f33cd6e7313074f5a83944efa10eeadd5c
Abubakar kehinde <abubakarkehinde88@gmail.com>
2024-05-11 13:33:13 +0100

    init commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 import { HiMenuAlt4 } from "react-icons/hi";
-import { AioOutlineClose } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
 import { kenny } from "../assets";
 import Image from "next/image";
 import { useState } from "react";
@@ -23,7 +23,7 @@ const Navbar = () => {
       </ul>
       <div>
         {toggleMenu ? (
-          <AioOutlineClose
+          <AiOutlineClose
             fontsize={28}
             className="text-white md:hidden cursor-pointer"
             onClick={() => setToggleMenu(true)}
@@ -36,8 +36,13 @@ const Navbar = () => {
           />
         )}
         {toggleMenu && (
-          <ul>
-            <AioOutlineClose onClick={() => setToggleMenu(false)} />
+          <ul className="z-10 fixed top-0 right-2 p-3 w-[70vw] h-screen shadow-xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
+            <li className="text-xl w-full my-2">
+              <AiOutlineClose onClick={() => setToggleMenu(false)} />
+            </li>
+            {["Market", "Exchange", "Rates", "Trade", "Account"].map((item, index) => (
+              <NavbarItems key={item + index} title={item} classProps="my-2 text-lg"/>
+            ))}
           </ul>
         )}
       </div>
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+const commonStyles =
+  "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] boder-gray-400 text-white";
 
 const Welcome = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <div className="flex w-full justify-center items-center">
+      <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col mf:mr-10">
+          <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+            Send Crypto <br />
+            Across The World
+          </h1>
+          <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+            Explore the crypto world with Dlt Africa web-3 club
+          </p>
+          <button className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
+            <p className="text-white text-base font-semibold">connect wallet</p>
+          </button>
+          <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+            <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
+            <div className={`${commonStyles}`}>Security</div>
+            <div className={`rounded-tr-2xl ${commonStyles}`}>Ethereum</div>
+            <div className={`rounded-bl-2xl ${commonStyles}`}>Easy Send</div>
+            <div className={`${commonStyles}`}>Low Fees</div>
+            <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
+          </div>
+        </div>
 
-export default Welcome
+        <div>
+          <div className="flex flex-col flex-1 items-start justify-start w-full mf:mt-o mt-10">
+            <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+              <div className="flex justify-between flex-col w-full h-full">
+                <div className="flex justify-between items-start">
+                  <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                    <SiEthereum fontSize={21} color="#fff" />
+                  </div>
+                  <BsInfoCircle fontSize={17} color="#fff" />
+                </div>
+              </div>
+              <p>
+              0xD1f4c4.....bef1e
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
