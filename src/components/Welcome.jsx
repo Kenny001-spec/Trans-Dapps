f8637c90d8b619dc533a0e10c5bf731a4e670441
Abubakar kehinde <abubakarkehinde88@gmail.com>
2024-05-13 14:10:54 +0100

    done

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,6 +5,15 @@ import { BsInfoCircle } from "react-icons/bs";
 const commonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] boder-gray-400 text-white";
 
+
+const Input = ({placeholder, name, type, value, handleChange }) => {
+  <input placeholder={placeholder}
+  type={type}
+  step="0.0001"
+  value={value}
+  // onChange={(e) => handleChange(e, name)}
+  className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm"/>
+}
 const Welcome = () => {
   return (
     <div className="flex w-full justify-center items-center">
@@ -30,21 +39,34 @@ const Welcome = () => {
           </div>
         </div>
 
-        <div>
-          <div className="flex flex-col flex-1 items-start justify-start w-full mf:mt-o mt-10">
-            <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
-              <div className="flex justify-between flex-col w-full h-full">
-                <div className="flex justify-between items-start">
-                  <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
-                    <SiEthereum fontSize={21} color="#fff" />
-                  </div>
-                  <BsInfoCircle fontSize={17} color="#fff" />
+        <div className="flex flex-col flex-1 items-start justify-start w-full mf:mt-o mt-10">
+          <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+            <div className="flex justify-between flex-col w-full h-full">
+              <div className="flex justify-between items-start">
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                  <SiEthereum fontSize={21} color="#fff" />
                 </div>
+                <BsInfoCircle fontSize={17} color="#fff" />
               </div>
-              <p>
-              0xD1f4c4.....bef1e
-              </p>
+
+              <div>
+                <p className="text-black font-bold text-lg ">
+                  0xD1f4c42.....bef1e
+                </p>
+                <p className="text-white font-semibold text-lg mt-1">
+                  Ethereum
+                </p>
+              </div>
             </div>
+          </div>
+
+          <div>
+            <Input 
+            placeholder="Wallet Address"
+            name='Address To'
+            type="Text"
+            // handleChange={handleChange}
+            />
           </div>
         </div>
       </div>
